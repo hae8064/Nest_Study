f2701d0afa629b6df9e0fea52d2aa6a06955bcd7
hae8064 <hae8064@naver.com>
2024-06-25 00:05:10 +0900

    feat: jwt token을 활용한 auth 로직 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,53 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Headers, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('token/access')
+  postTokenAccess(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, false);
+    /**
+     * 아래와 같은 형태로 반환을 원함
+     * {accessToken: {token}}
+     */
+
+    return {
+      accessToken: newToken,
+    };
+  }
+  @Post('token/refresh')
+  postTokenRefresh(@Headers('authorization') rawToken: string) {
+    //토큰은 bearer토큰이므로 param 두번째에 true로 변환
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, true);
+    /**
+     * 아래와 같은 형태로 반환을 원함
+     * {accessToken: {token}}
+     */
+
+    return {
+      refreshToken: newToken,
+    };
+  }
+
   @Post('login/email')
-  loginEmail(@Body('email') email: string, @Body('password') password: string) {
-    return this.authService.loginWithEmail({
-      email,
-      password,
-    });
+  postLoginEmail(@Headers('authorization') rawToken: string) {
+    // email:password 값이 -> base64로 인코딩 되어있음
+    // ex) dsadasdas:dsafsfas 이러한 형태로 인코딩 되어 있음
+    const token = this.authService.extractTokenFromHeader(rawToken, false);
+
+    const credentials = this.authService.decodeBasicToken(token);
+
+    return this.authService.loginWithEmail(credentials);
   }
 
   @Post('register/email')
-  registerEmail(
+  postRegisterEmail(
     @Body('nickname') nickname: string,
     @Body('email') email: string,
     @Body('password') password: string,
