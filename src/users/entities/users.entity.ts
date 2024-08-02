a0a5582647f78e03c6da1606cd9360caab9456ec
hae8064 <hae8064@gmail.com>
2024-08-02 23:50:59 +0900

    feat: exception filter 추가

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -2,18 +2,11 @@ import { Column, Entity, OneToMany } from 'typeorm';
 import { RolesEnum } from '../const/roles.const';
 import { PostsModel } from 'src/posts/entities/posts.entity';
 import { BaseModel } from 'src/common/entity/base.entity';
-import {
-  IsEmail,
-  IsString,
-  length,
-  Length,
-  min,
-  ValidationArguments,
-} from 'class-validator';
+import { IsEmail, IsString, Length } from 'class-validator';
 import { lengthValidationMessage } from 'src/common/validation-message/length-validation.message';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { emailValidationMessage } from 'src/common/validation-message/email-validation.message';
-import { Exclude, Expose } from 'class-transformer';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class UsersModel extends BaseModel {
